ccae462c05bac4155421c6bd4a4e733b64592fb5
nalugao <nalugao@hotmail.com>
2026-02-17 16:11:42 -0300

    Ajusta nome das pastas e inclui nova aplicação

---
+++ src/cliGoogleBooksAPI/Main.java
@@ -0,0 +1,24 @@
+package cliGoogleBooksAPI;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o livro que você procura: ");
+        var titulo = sc.nextLine();
+        URI uri = new URI("https://www.googleapis.com/books/v1/volumes?q=" + URLEncoder.encode(titulo, StandardCharsets.UTF_8.toString()));
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(uri).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
--- src/coinGeckoAPI/Main.java
+++
@@ -1,4 +0,0 @@
-package coinGeckoAPI;
-
-public class Main {
-}
